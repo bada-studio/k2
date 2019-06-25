11ca800878f060b135fdbcb06416349748f3d0c8
jblee84 <jb.lee@biscuitlabs.io>
2019-06-25 14:19:23 +0900

    ..

--- src/App.js
+++ src/App.js
@@ -35,6 +35,7 @@ class App extends Component {
         <MetaTags>
           <title>클레이튼 나이츠 : Klaytn Knights</title>
           <meta name="google-site-verification" content="cb0xW8VuDJ6cA0crHJfg9QAN7YrDYhZLLJkJo1eaG1E" />
+          <meta name="naver-site-verification" content="065e3c27ed26edbb9f2f5415691f03fe16a055da"/>
           <meta name="description" content="클레이튼 나이츠는 누구나 즐길 수 있는 카카오 최초의 블록체인 게임입니다."/>
           <meta property="og:title" content="클레이튼 나이츠 : Klaytn Knights" />
           <meta property="og:image" content="./../../Images/title_v0.png" />
